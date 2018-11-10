1bdea5b0818a5e4cf81ab30e17b02691f9c3447e
root <klug123@gmail.com>
2018-11-10 10:21:45 -0200

    CRUD completo para garagem.

--- laravel/src/public/js/garagem.js
+++ laravel/src/public/js/garagem.js
@@ -8,8 +8,14 @@ function buscaDados(){
         $.each(data, function(key, val){
             tabelaHtml+="<tr id='"+val.id+"'><td>"+val.id+"</td>"+
                         "<td>"+val.nome+"</td>"+
-                        "<td>"+val.telefone+"</td></tr>";
+                        "<td>"+val.telefone+"</td>"+
+                        "<td> <a class='btn' id='BtnAlteraGaragem' onclick='alterarGaragem("+val.id+")')><i class='fa fa-edit'></i></a>"+
+                        "<a class='btn' id='BtnExcluiGaragem' onclick='deletarGaragem("+val.id+")')><i class='fa fa-trash'></i></a>"+
+                        "</td></tr>";
         });
+        //limpa tabela de garagens
+        $("#tabelaGaragens td").parent().remove();
+        //preenche a tabela de garagens
         $("#tabelaGaragens").append(tabelaHtml);
     });
 };
@@ -19,8 +25,46 @@ function adicionarGaragem(){
         type: "POST",
         url: "http://localhost:41121/api/garagem",
         data: 'nome='+$("#nomeGaragem").val()+'&telefone='+$("#telefoneGaragem").val()
-    })
-}
+    }).then(res=>{
+        $("#cadastrarForm").modal("hide");
+        buscaDados();
+
+    });
+};
+
+function deletarGaragem(id){
+    $.ajax({
+        type: "DELETE",
+        url: "http://localhost:41121/api/garagem/"+id,
+    }).then(res=>{
+        buscaDados();
+    });
+};
+
+function alterarGaragem(id){
+    $.getJSON("http://localhost:41121/api/garagem/"+id, function(data, status) {
+        var garagemAltera = [];
+        $.each(data, function(key, val) {
+            garagemAltera.push(val);
+        });
+        console.log(garagemAltera);
+        $("#alterarForm").modal("show");
+        $("#idGaragemAltera").val(garagemAltera[0]);
+        $("#nomeGaragemAltera").val(garagemAltera[4]);
+        $("#telefoneGaragemAltera").val(garagemAltera[3]);
+    });
+    $("#alterar").click(function(){
+        $.ajax({
+            type: "PUT",
+            url: "http://localhost:41121/api/garagem/"+id,
+            data: 'nome='+$("#nomeGaragemAltera").val()+'&telefone='+$("#telefoneGaragemAltera").val()
+        }).then(res=>{
+            $("#alterarForm").modal("hide");
+            buscaDados();
+        });
+    });
+};
+
 
 /*
 $("#cadastrar").click(function() {
--- laravel/src/resources/views/garagem.blade.php
+++ laravel/src/resources/views/garagem.blade.php
@@ -5,39 +5,48 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <title>Lista de Garagens</title>
     
 </head>
 <body>
+
+    <!--tabela de garagens-->
     <table class="table table-striped" id="tabelaGaragens">
         <tr>
             <thead>
                 <th>ID da Garagem</th>
                 <th>Nome da Garagem</th>
                 <th>Telefone da Garagem</th>
+                <th>Opções</th>
             </thead>
         </tr>
     </table>
 
+    <!--botão para inserir nova garagem-->
     <a href="" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#cadastrarForm">Cadastrar nova garagem</a>
-    <!--
-        modal para inserir nova garagem
-    -->
+
+    <!--modal para inserir nova garagem-->
     <div class="modal fade" id="cadastrarForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
+
                 <div class="modal-content">
+
                     <div class="modal-header text-center">
-                        <h4 class="modal-title w-100 font-weight-bold">Cadastrar</h4>
+                        <h4 class="modal-title w-100 font-weight-bold">Cadastrar Garagem</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
+
                     <div class="modal-body mx-3">
+
                         <div class="md-form mb-5">
                             <label data-error="wrong" data-success="right" for="nomeGaragem">Nome</label>
                             <i class="fa fa-user prefix grey-text"></i>
                             <input type="text" id="nomeGaragem" class="form-control validate">
-                        </div>
+                        </div><br>
+
                         <div class="md-form mb-4">
                             <label data-error="wrong" data-success="right" for="telefoneGaragem">Telefone</label>
                             <i class="fa fa-phone prefix grey-text"></i>
@@ -45,11 +54,56 @@
                         </div>
         
                     </div>
+
                     <div class="modal-footer d-flex justify-content-center">
                         <button class="btn btn-deep-orange" id='cadastrar' onclick='adicionarGaragem()'>Cadastrar</button>
                     </div>
+
+                </div>
+
+            </div>
+    </div>
+
+    <!--modal para alterar garagem-->
+    <div class="modal fade" id="alterarForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+
+                <div class="modal-content">
+
+                    <div class="modal-header text-center">
+                        <h4 class="modal-title w-100 font-weight-bold">Alterar Garagem</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+
+                    <div class="modal-body mx-3">
+                        <div class="md-form mb-5">
+                            <label data-error="wrong" data-success="right" for="idGaragemAltera">ID</label>
+                            <i class="fa fa-hashtag prefix grey-text"></i>
+                            <input type="text" id="idGaragemAltera" readonly class="form-control validate">
+                        </div><br>
+
+                        <div class="md-form mb-5">
+                            <label data-error="wrong" data-success="right" for="nomeGaragemAltera">Nome</label>
+                            <i class="fa fa-user prefix grey-text"></i>
+                            <input type="text" id="nomeGaragemAltera" class="form-control validate">
+                        </div><br>
+
+                        <div class="md-form mb-4">
+                            <label data-error="wrong" data-success="right" for="telefoneGaragemAltera">Telefone</label>
+                            <i class="fa fa-phone prefix grey-text"></i>
+                            <input type="text" id="telefoneGaragemAltera" class="form-control validate">
+                        </div>
+                    </div>
+
+                    <div class="modal-footer d-flex justify-content-center">
+                        <button class="btn btn-deep-orange" id='alterar'>Alterar Dados</button>
+                    </div>
+
                 </div>
             </div>
+
     </div>
 </body>
 
